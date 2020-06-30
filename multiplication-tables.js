c0e47e9619ced329bde2ce638323490a845e4ee1
Ruben80 <43403734+Ruben80@users.noreply.github.com>
2020-06-30 13:03:45 +0200

    Add files via upload

--- multiplication-tables.js
+++ multiplication-tables.js
@@ -1,8 +1,26 @@
-for (let i = 1; i <= 10; i++) {
-    count = i * 9 
-    
-    console.log(i + " X 9 = " + count); 
+for (let multi = 1; multi <= 10; multi++) {
+  for (let i = 1; i <= 10; i++) {
+    count = i * multi;
+    console.log(i + " X " + multi + " = " + count);
+  }
 };
+
+
+// // Bonus
+// for (let multiplier = 0; multiplier <= 10; multiplier++) {
+//     for (let i = 0; i <= 10; i++) {
+//       let result = multiplier * i;
+//       console.log(multiplier + " * " + i + " = " + result);
+//     }
+//   }
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     count = i * 9
+
+//     console.log(i + " X 9 = " + count);
+// };
 
 // let i;
 
@@ -10,7 +28,7 @@ for (let i = 1; i <= 10; i++) {
 //     if (i % 2 === 0) {
 //         console.log(i + " is even");
 //     } else {
-//         console.log(i + " is oneven") 
+//         console.log(i + " is oneven")
 //     }
 // };
 
