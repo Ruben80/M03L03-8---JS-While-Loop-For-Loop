396f5cfb38b4673d35b3d33ca41aa77643108716
Ruben80 <43403734+Ruben80@users.noreply.github.com>
2020-06-30 11:53:17 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,17 @@
+let i;
+
+for (i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is even");
+    } else {
+        console.log(i + " is oneven") 
+    }
+};
+
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i + " is even");
+//     } else {
+//       console.log(i + " is oneven");
+//     }
+//   }
